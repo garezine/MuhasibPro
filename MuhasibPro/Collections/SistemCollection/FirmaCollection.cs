61d41aa160fc397a058b24e614fa810d05c864bf
garezine <korkutomer@gmail.com>
2025-09-23 23:23:11 +0300

    düzenleme

---
+++ MuhasibPro/Collections/SistemCollection/FirmaCollection.cs
@@ -0,0 +1,66 @@
+﻿using Muhasebe.Business.Models.SistemModel;
+using Muhasebe.Business.Services.Abstracts.Common;
+using Muhasebe.Domain.Entities.SistemEntity;
+using Muhasebe.Domain.Enum;
+using Muhasebe.Domain.Helpers;
+using MuhasibPro.Infrastructure.VirtualCollection;
+using MuhasibPro.ViewModels.Contracts.SistemServices;
+
+namespace MuhasibPro.Collections.SistemCollection
+{
+    public class FirmaCollection : VirtualCollection<FirmaModel>
+    {
+        private DataRequest<Firma> _dataRequest = null;
+        bool MustExploreDeepExceptions { get; set; }
+        public FirmaCollection(IFirmaService firmaService, ILogService logService, bool mustExploreDeepExceptions = false) : base(logService)
+        { FirmaService = firmaService; }
+
+        public IFirmaService FirmaService { get; }
+
+        private FirmaModel _defaultItem = FirmaModel.CreateEmpty();
+
+        public override FirmaModel DefaultItem => _defaultItem;
+
+        public async Task LoadAsync(DataRequest<Firma> dataRequest)
+        {
+            try
+            {
+                _dataRequest = dataRequest;
+
+                Count = await FirmaService.GetFirmalarCountAsync(_dataRequest);
+                var range = await FirmaService.GetFirmalarAsync(0, RangeSize, _dataRequest);
+                Ranges[0] = range.Data;
+            }
+            catch (Exception)
+            {
+                Count = 0;
+                throw;
+            }
+        }
+
+        public override async Task<IList<FirmaModel>> FetchDataAsync(int pageIndex, int pageSize)
+        {
+            try
+            {
+                var fetch = await FirmaService.GetFirmalarAsync(pageIndex * pageSize, pageSize, _dataRequest);
+                return fetch.Data;
+            }
+            catch (Exception ex)
+            {
+                LogException("Firma Koleksiyon", "Getir", ex);
+            }
+            return null;
+        }
+        protected async void LogException(string source, string action, Exception exception)
+        {
+            if (MustExploreDeepExceptions == false)
+            {
+                await LogService.SistemLogService.WriteAsync(LogType.Hata, source, action, exception.Message, exception.ToString());
+            }
+            else
+            {
+                await LogService.SistemLogService.WriteAsync(LogType.Hata, source, action, exception);
+            }
+        }
+    }
+}
---
+++ MuhasibPro/Controls/Forms/FormTypes.cs
@@ -0,0 +1,48 @@
+﻿namespace MuhasibPro.Controls;
+
+public interface IFormControl
+{
+    event EventHandler<FormVisualState> VisualStateChanged;
+
+    FormEditMode Mode
+    {
+        get;
+    }
+    FormVisualState VisualState
+    {
+        get;
+    }
+
+    bool IsEnabled
+    {
+        get;
+    }
+
+    bool Focus(FocusState value);
+
+    void SetVisualState(FormVisualState visualState);
+}
+
+public enum TextDataType
+{
+    String,
+    Integer,
+    Decimal,
+    Double
+}
+
+public enum FormEditMode
+{
+    Auto,
+    ReadOnly,
+    ReadWrite
+}
+
+public enum FormVisualState
+{
+    Idle,
+    Ready,
+    Focused,
+    Disabled
+}
+
